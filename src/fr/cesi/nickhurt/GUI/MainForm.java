d605263e62e3de6309fccfac67cd54ca90a8ea03
NHURTEVENT <nicolas.hurtevent@viacesi.fr>
2020-04-02 02:13:59 +0200

    find key in 40 min with frequency analysis and dictionary

--- src/fr/cesi/nickhurt/GUI/MainForm.java
+++ src/fr/cesi/nickhurt/GUI/MainForm.java
@@ -9,30 +9,39 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class MainForm extends JFrame{
     public String DICTIONARY_PATH = "E:\\Users\\Nico\\Desktop\\projet crypto\\Ressources\\liste_francais.txt";
+    public String KEYS_FILEPATH = "E:\\Users\\Nico\\Desktop\\projet crypto\\keys.txt";
     private JPanel mainPanel;
     private JPanel filePanel;
     private DropTarget dropTarget;
     private DropTargetHandler dropTargetHandler;
     private JLabel keyLabel;
-    private JPanel inputPanel;
     private JTextField keyField;
     private JButton DecypherButton;
     private JButton explorerButton;
-    private JLabel noKeyLAbel;
+    private JLabel noKeyLabel;
     private JButton bruteForceButton;
     private JFileChooser fc;
     private String filePath;
     private byte[] fileContent;
+    public int nbOfDictionaryRequests;
+    public int nbOfDictionaryComparisons;
 
     public MainForm() {
         //filePanel = new FilePanel();
-        JFrame frame = new JFrame("Déchiffrator3000");
+        JFrame frame = new JFrame("Dehacking tool");
         frame.setContentPane(mainPanel);
         mainPanel.setLayout(new GridLayout(10,1));
         //mainPanel.add(filePanel);
@@ -70,6 +79,7 @@ public class MainForm extends JFrame{
         DecypherButton.addActionListener(e -> {
             System.out.println("open explorer");
             String decodedMessage = decodeByte(keyField.getText(), fileContent);
+            frequencyPretest(decodedMessage);
             write(decodedMessage);
             if (testFileCoherence(decodedMessage)) {
                 System.out.println("File deciphered with success");
@@ -77,10 +87,70 @@ public class MainForm extends JFrame{
         });
 
         bruteForceButton.addActionListener(e -> {
-            String[] keys = generateKeys(6);
-            for(String key: keys) {
+            //testDecodeTime();
+//            String[] keys = generateKeys(6);
+            //TODO decrypt while testing so as to not decrypt a whole file for nothing
+//            for(int i =98689000; i< getNumberOfKeys(6); i++) {
+            Instant start = Instant.now();
+//            for(int i =98680000; i< getNumberOfKeys(6); i++) {
+            String alphabet = "abcdefghijklmnopqrstuvwxyz";
+//            for(int i =309000000; i< getNumberOfKeys(6); i++) {
+            for(int i = 0; i< getNumberOfKeys(6); i++) {
+//            for(int i =0; i< getNumberOfKeys(6); i++) {
+                String key = getKey(i, alphabet);
+                //System.out.println(key);
+                String decodedSubMessage = decodeByte(key, Arrays.copyOfRange(fileContent, 0, 150));
+                if(i%500000 ==0) {
+                    System.out.println("tried "+i+" keys");
+                    Instant currentTime = Instant.now();
+                    Duration timeElapsed = Duration.between(start, currentTime);
+                    System.out.println(nbOfDictionaryRequests+" dictionary requests");
+                    System.out.println(nbOfDictionaryComparisons +" dictionary comparisons");
+                    System.out.println("Time taken: "+ timeElapsed.toMillis()/1000 +" seconds");
 
+                }
+
+                if(!frequencyPretest(decodedSubMessage)) {
+//                    System.out.println("continue");
+                    continue;
+                }
+
+                //TODO je vais surement juste décoder les 200-500-1k premiers chars anyway
+                //String decodedMessage = decodeByte(key, fileContent);
+                String decodedMessage = decodedSubMessage;
+                if (testFileCoherence(decodedMessage)) {
+                    System.out.println("File deciphered with success. key :"+key);
+                    Instant currentTime = Instant.now();
+                    Duration timeElapsed = Duration.between(start, currentTime);
+                    System.out.println("tried "+i+" keys");
+                    System.out.println(nbOfDictionaryRequests+" dictionary requests");
+                    System.out.println(nbOfDictionaryComparisons +" dictionary comparisons");
+                    System.out.println("Time taken: "+ timeElapsed.toMillis()/1000 +" seconds");
+                    write(decodedMessage);
+                    break;
+                } else {
+                    if(i%1000 ==0) {
+                        System.out.println("wrong key: "+key);
+                        System.out.println("tried "+i+" keys");
+                        System.out.println(nbOfDictionaryRequests+" dictionary requests");
+                        System.out.println(nbOfDictionaryComparisons +" dictionary comparisons");
+                    }
+                    if(i%10000 == 0) {
+                        Instant currentTime = Instant.now();
+                        Duration timeElapsed = Duration.between(start, currentTime);
+                        System.out.println("Time taken: "+ timeElapsed.toMillis()/1000 +" seconds");
+                    }
+                }
             }
+//            for(String key: keys) {
+//                String decodedMessage = decodeByte(key, fileContent);
+//                if (testFileCoherence(decodedMessage)) {
+//                    System.out.println("File deciphered with success");
+//                    write(decodedMessage);
+//                } else {
+//                    System.out.println("wrong key: "+key);
+//                }
+//            }
         });
 
         explorerButton.addActionListener(e -> {
@@ -98,6 +168,21 @@ public class MainForm extends JFrame{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void testDecodeTime() {
+        Instant start = Instant.now();
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < getNumberOfKeys(6); i++) {
+            String key = getKey(i, alphabet);
+            String decodedMessage = decodeByte(key, Arrays.copyOfRange(fileContent, 0, 100));
+            if(i%10000 ==0) {
+                System.out.println("tried "+i+" keys");
+                Instant currentTime = Instant.now();
+                Duration timeElapsed = Duration.between(start, currentTime);
+                System.out.println("Time taken: "+ timeElapsed.toMillis()/1000 +" seconds");
+            }
+        }
     }
 
     private DropTarget getMyDropTarget() {
@@ -118,7 +203,7 @@ public class MainForm extends JFrame{
         File file = new File(path);
         try {
             byte[] fileContent = Files.readAllBytes(file.toPath());
-            System.out.println(Arrays.toString(fileContent));
+            //System.out.println(Arrays.toString(fileContent));
             return fileContent;
         } catch (IOException e) {
             e.printStackTrace();
@@ -166,14 +251,14 @@ public class MainForm extends JFrame{
             byte keyChar = (byte)longKey.charAt(j);
             int keyCharInt = longKey.charAt(j);
             if(codedChar < 0) {
-                System.out.println(codedChar);
+                //System.out.println(codedChar);
                 int positive = codedChar*-1;
                 int unsigned = positive+127;
-                System.out.println(positive);
-                System.out.println(unsigned);
+                //System.out.println(positive);
+                //System.out.println(unsigned);
                 int codedCharInt = (codedChar+256);
                 int xoredCharInt = codedCharInt^keyCharInt;
-                System.out.println(xoredCharInt+" "+(char)xoredCharInt);
+                //System.out.println(xoredCharInt+" "+(char)xoredCharInt);
                 xorString+=(char)xoredCharInt;
             } else {
                 byte xoredChar =(byte) (codedChar ^keyChar);
@@ -181,7 +266,7 @@ public class MainForm extends JFrame{
             }
 
         }
-        System.out.println(xorString);
+        //System.out.println(xorString);
         return xorString;
     }
 
@@ -233,7 +318,7 @@ public class MainForm extends JFrame{
     }
 
     public void write(String fileContent) {
-        System.out.println("gotta print"+fileContent);
+        //System.out.println("gotta print"+fileContent);
         String outputFile = filePath.replace(".txt","Decoded.txt");
         BufferedWriter writer;
         try {
@@ -244,8 +329,24 @@ public class MainForm extends JFrame{
             e.printStackTrace();
         }
     }
+
     public void bruteForce(ArrayList<Integer> encodedMessage) {
 
+    }
+
+    public String getKey(int i, String alphabet) {
+//        return i < 0 ? "" : getKey((i / 26) - 1) + (char) (97 + i % 26);
+//        char[] alphabet = new String("abcdefghijklmnopqrstuvwxyz");
+        int alphabetSize = alphabet.length();
+        return i < 0 ? "" : getKey((i / alphabetSize)-1, alphabet) + alphabet.toCharArray()[i % 26];
+    }
+
+    public int getNumberOfKeys(int maxKeyLength) {
+        int numberOfKeys = 0;
+        for(int i =1; i<= maxKeyLength; i++) {
+            numberOfKeys += Math.pow(26,i);
+        }
+        return  numberOfKeys;
     }
 
     public String[] generateKeys(int maxKeyLength) {
@@ -255,6 +356,22 @@ public class MainForm extends JFrame{
         }
         System.out.println(numberOfKeys+" possible keys");
         String[] keys = new String[numberOfKeys];
+        String fileName = KEYS_FILEPATH;
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        try (FileOutputStream fos = new FileOutputStream(fileName, true)) {
+
+        for(int i =0; i< numberOfKeys; i++) {
+            String key = getKey(i, alphabet);
+            keys[i] = key;
+            System.out.println(keys[i]);
+            fos.write((key+"\n").getBytes());
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return keys;
     }
 
@@ -264,7 +381,7 @@ public class MainForm extends JFrame{
 
     public boolean testFileCoherence(String decodedMessage, String[] dictionaryWords) {
 //        String sample = decodedMessage.substring(0,300);
-        String[] words = decodedMessage.split(" |\\.|,|'|\\(|\\)|\\n|\\r");
+
         int invalidChars = 0;
         int totalChars = 0;
         int invalidWords = 0;
@@ -272,38 +389,52 @@ public class MainForm extends JFrame{
         int totalWords = 0;
         int matchedWords = 0;
         int unmatchedWords = 0;
+//        Map<Integer, Long> lettersCount = countLetters(words);
+//
+//        for( Map.Entry<Integer, Long> entry: lettersCount.entrySet()){
+//            System.out.println((char)entry.getKey().intValue());
+//            System.out.println(entry.getKey().longValue());
+//        }
+
+        String[] words = decodedMessage.split(" |\\.|,|'|\\(|\\)|\\n|\\r");
+        if(words.length < 6) {
+//            System.out.println("less than 5 words in file, unlikely");
+            return false;
+        }
         for(String word : words) {
             int invalidCharInWord = 0;
             boolean isInvalid = false;
             for(char letter: word.toCharArray()) {
                 totalChars++;
                 if((int) letter > 255 || (int) letter < 0 || !(""+letter).matches("[A-zÀ-ú]*[?\\-]*")) {
-                    System.out.println("invalid char "+letter+" in word "+ word);
+                    //System.out.println("invalid char "+letter+" in word "+ word);
                     invalidCharInWord++;
                     invalidChars++;
                 }
                 if(invalidCharInWord > 1) {
-                    System.out.println("2 invalid char in word "+word+" , breaking");
+                    //System.out.println("2 invalid char in word "+word+" , breaking");
                     invalidWords++;
                     isInvalid = true;
                     break;
                 }
+
             }
             if(invalidWords > totalWords/10 && invalidChars > 9) { //could see the frequency of é,è,ç,à in french to establish the right percentage
-                System.out.println(invalidWords+"/"+totalWords);
-                System.out.println("over 10% invalid words, wrong key, return");
+                //System.out.println(invalidWords+"/"+totalWords);
+                //System.out.println("over 10% invalid words, wrong key, return");
                 return false;
             } else {
                 if(!isInvalid && word.length() > 3) {
                     validWords++;
-                    System.out.println("will compare with dictionary :"+ word);
+                    //System.out.println("will compare with dictionary :"+ word);
                     boolean matched = compareToDictionary(word);
+//                    boolean matched = compareToTextFile(word);
                     if(matched) matchedWords++; else unmatchedWords++;
-                    System.out.println(matched+" matched = "+matchedWords+" unmatched= "+unmatchedWords);
+                    //System.out.println(matched+" matched = "+matchedWords+" unmatched= "+unmatchedWords);
 
 
                     if(validWords > 10 && (float)matchedWords/validWords < 0.3) {
-                        System.out.println("didn't match enough words, key discarded");
+                        //System.out.println("didn't match enough words, key discarded");
                         return false;
                     } else if (validWords > 10 && (float)matchedWords/validWords > 0.5){
                         System.out.println("over 50% of the words matched, found the key !");
@@ -314,19 +445,57 @@ public class MainForm extends JFrame{
             }
         }
         System.out.println("done: "+totalChars+" chars, "+invalidChars+" invalid chars ("+(float)invalidChars/totalChars+"%), "+invalidWords+" invalid words ("+(float)invalidWords/words.length+"%)");
-        return true;
+        System.out.println("Could no conclude on file validity.");
+        return false;
+    }
+
+    public boolean frequencyPretest(String message) {
+
+        String testSubset = message.substring(0,150);
+        String[] words = testSubset.split(" |\\.|,|'|\\(|\\)|\\n|\\r");
+        Map<Integer, Long> letterCount = countLetters(words);
+
+        if(!(letterCount.containsKey(101) && letterCount.containsKey(115) && letterCount.containsKey(97))) {
+            return false;
+        }
+
+        long numberOfE = letterCount.get(101);
+        long numberOfS = letterCount.get(115);
+        long numberOfA = letterCount.get(97);
+
+        float percentageE = (float)numberOfE/150; // 14.7% in french
+        float percentageS = (float)numberOfS/150; // 7.9% in french
+        float percentageA = (float)numberOfA/150; // 7.6% in french
+        float totalPercentage = percentageE+ percentageS+ percentageA; // 30.2% in french
+
+        if(totalPercentage<0.2) { //50% uncertainty should be plenty to avoid false negative
+            return false;
+        } else {
+//            System.out.println("OVER 20%"+totalPercentage);
+            return true;
+        }
+    }
+
+    public static Map<Integer, Long> countLetters(String[] words) {
+        return Arrays.stream(words)
+                .flatMapToInt(String::chars)
+                .filter(Character::isLetter)
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
     public boolean compareToDictionary(String wordToCompare) {
         String[] dictionary = getDictionary();
+        nbOfDictionaryRequests++;
         for( String dictionaryWord: dictionary) {
+            nbOfDictionaryComparisons++;
             String escapedWord = Pattern.quote(dictionaryWord);
-            System.out.println(escapedWord);
+            //System.out.println(escapedWord);
             if(wordToCompare.matches(escapedWord)){
-                System.out.println(wordToCompare+" matched with dictionary word "+dictionaryWord);
+                //System.out.println(wordToCompare+" matched with dictionary word "+dictionaryWord);
                 return true;
             } else {
-                System.out.println(wordToCompare+" didn't match with dictionary word "+dictionaryWord);
+                //System.out.println(wordToCompare+" didn't match with dictionary word "+dictionaryWord);
             }
         }
         return false;
@@ -334,6 +503,7 @@ public class MainForm extends JFrame{
 
     //TODO implement a DAO
     public boolean compareToTextFile(String wordToCompare) {
+        nbOfDictionaryRequests++;
         return false;
     }
 
